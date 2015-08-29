8deb617819782fc7183ec284b39c69456ad06e21
Tuss4 <tuss4dbfn@gmail.com>
2015-08-29 17:02:20 -0500

    Using json package.

--- main.go
+++ main.go
@@ -1,27 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
+type slackResp struct {
+	Text string
+}
+
 var (
 	port     = ":8000"
-	commands = map[string]string{
-		"ping":     "PING FIVE! WHOO-PSH! SNAP!",
-		"betrayal": "https://www.youtube.com/watch?v=uw5P2up5bL4",
-		"miracle":  "https://www.youtube.com/watch?v=uehf8e43Vtk",
-		"meow":     "https://www.youtube.com/watch?v=xRhATB9NelU",
+	commands = map[string]slackResp{
+		"ping":     slackResp{"PING FIVE! WHOO-PSH! SNAP!"},
+		"betrayal": slackResp{"<https://www.youtube.com/watch?v=uw5P2up5bL4>"},
+		"miracle":  slackResp{"<https://www.youtube.com/watch?v=uehf8e43Vtk>"},
+		"meow":     slackResp{"<https://www.youtube.com/watch?v=xRhATB9NelU>"},
 	}
-	notFound = "That command was bad and you should feel bad."
+	notFound = slackResp{"That command was bad and you should feel bad."}
 )
 
 func handleQuery(res http.ResponseWriter, req *http.Request) {
-	qMap := req.URL.Query()
-	fmt.Println(qMap)
 	res.Header().Set("Content-Type", "application/json")
-	io.WriteString(res, `{"foo": "bar"}`)
+	qMap := req.URL.Query()
+	qVal := qMap["q"][0]
+	if commands[qVal] != (slackResp{}) {
+		data, err := json.Marshal(commands[qVal])
+		if err != nil {
+			log.Fatal(err)
+		}
+		io.WriteString(res, string(data))
+	} else {
+		data, err := json.Marshal(notFound)
+		if err != nil {
+			log.Fatal(err)
+		}
+		io.WriteString(res, string(data))
+	}
 }
 
 func main() {
