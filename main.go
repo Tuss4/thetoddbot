4682688b0bfa8082713c58703be2175cc8cb96a9
Tuss4 <tuss4dbfn@gmail.com>
2015-08-30 00:18:06 -0500

    Done. Going to write some tests.

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -10,57 +9,44 @@ import (
 	"os"
 )
 
-type slackResp struct {
-	Text string
-}
-
 var (
 	port     = ":8000"
-	commands = map[string]slackResp{
-		"ping":     slackResp{"PING FIVE! WHOO-PSH! SNAP!"},
-		"betrayal": slackResp{"<https://www.youtube.com/watch?v=uw5P2up5bL4>"},
-		"miracle":  slackResp{"<https://www.youtube.com/watch?v=uehf8e43Vtk>"},
-		"meow":     slackResp{"<https://www.youtube.com/watch?v=xRhATB9NelU>"},
+	commands = map[string]string{
+		"ping":     `{"text": "PING FIVE! WHOO-PSH! SNAP!"}`,
+		"betrayal": `{"text": "<https://www.youtube.com/watch?v=uw5P2up5bL4>"}`,
+		"miracle":  `{"text": "<https://www.youtube.com/watch?v=uehf8e43Vtk>"}`,
+		"meow":     `{"text": "<https://www.youtube.com/watch?v=xRhATB9NelU>"}`,
 	}
-	notFound = slackResp{"That command was bad and you should feel bad."}
+	notFound = `{"text": "That command was bad and you should feel bad."}`
 	hookURL  = os.Getenv("SLACK_WEBHOOK_URL")
 )
 
-func handleHook(res http.ResponseWriter, req *http.Request) {
+func handleHook(res http.ResponseWriter, req *http.Request, val string) {
 	client := &http.Client{}
-	the_string := `{"text": "<https://www.youtube.com/watch?v=uehf8e43Vtk>"}`
-	print(hookURL)
-	req, err := http.NewRequest("POST", hookURL, bytes.NewBuffer([]byte(the_string)))
+	req, err := http.NewRequest("POST", hookURL, bytes.NewBuffer([]byte(val)))
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	print(resp.Status, resp.StatusCode)
+	fmt.Println("Status:", resp.Status)
 }
 
 func handleQuery(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
 	qMap := req.URL.Query()
 	qVal := qMap["q"][0]
-	if commands[qVal] != (slackResp{}) {
-		data, err := json.Marshal(commands[qVal])
-		if err != nil {
-			log.Fatal(err)
-		}
-		io.WriteString(res, string(data))
+	if commands[qVal] != "" {
+		handleHook(res, req, commands[qVal])
+		io.WriteString(res, `{"status": "Valid Command"}`)
 	} else {
-		data, err := json.Marshal(notFound)
-		if err != nil {
-			log.Fatal(err)
-		}
-		io.WriteString(res, string(data))
+		handleHook(res, req, notFound)
+		io.WriteString(res, `{"status": "Invalid Command"}`)
 	}
 }
 
 func main() {
 	fmt.Println("Server running listening on port", port)
-	http.HandleFunc("/hook", handleHook)
 	http.HandleFunc("/command", handleQuery)
 	http.ListenAndServe(port, nil)
 }
