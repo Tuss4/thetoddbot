b471a0723f21a4b2610e326e906289e50536333b
Tuss4 <tuss4dbfn@gmail.com>
2015-09-01 13:32:09 -0500

    Test all commands

--- todd_test.go
+++ todd_test.go
@@ -45,7 +45,23 @@ func TestCommandNotFound(t *testing.T) {
 	}
 }
 
+func TestMethodNotAllowed(t *testing.T) {
+	req, err := http.NewRequest("GET", "/command", nil)
+	w := httptest.NewRecorder()
+	if err != nil {
+		t.Fatal("Creating 'GET /command' request failed!")
+	}
+	commandHandler().ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Command page didn't return %v", http.StatusOK)
+	}
+	if w.Body.String() != notAllowed {
+		t.Errorf("Response body didn't return %v", notAllowed)
+	}
+}
+
 func TestCommandFound(t *testing.T) {
+	// Test "oneq"
 	data := url.Values{}
 	data.Set("token", getToken())
 	data.Add("text", "oneq")
@@ -62,6 +78,7 @@ func TestCommandFound(t *testing.T) {
 	if w.Body.String() != commands["oneq"] {
 		t.Errorf("Response body didn't return %v", commands["oneq"])
 	}
+	// Test "betrayal"
 	data.Del("text")
 	data.Add("text", "betrayal")
 	req, err = http.NewRequest("POST", "/command", bytes.NewBufferString(data.Encode()))
@@ -76,5 +93,53 @@ func TestCommandFound(t *testing.T) {
 	}
 	if w.Body.String() != commands["betrayal"] {
 		t.Errorf("Response body didn't return %v", commands["betrayal"])
+	}
+	// Test "ping"
+	data.Del("text")
+	data.Add("text", "ping")
+	req, err = http.NewRequest("POST", "/command", bytes.NewBufferString(data.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w = httptest.NewRecorder()
+	if err != nil {
+		t.Fatal("Creating 'POST /command' request failed!")
+	}
+	commandHandler().ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Command page didn't return %v", http.StatusOK)
+	}
+	if w.Body.String() != commands["ping"] {
+		t.Errorf("Response body didn't return %v", commands["ping"])
+	}
+	// Test "miracle"
+	data.Del("text")
+	data.Add("text", "miracle")
+	req, err = http.NewRequest("POST", "/command", bytes.NewBufferString(data.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w = httptest.NewRecorder()
+	if err != nil {
+		t.Fatal("Creating 'POST /command' request failed!")
+	}
+	commandHandler().ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Command page didn't return %v", http.StatusOK)
+	}
+	if w.Body.String() != commands["miracle"] {
+		t.Errorf("Response body didn't return %v", commands["miracle"])
+	}
+	// Test "meow"
+	data.Del("text")
+	data.Add("text", "meow")
+	req, err = http.NewRequest("POST", "/command", bytes.NewBufferString(data.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w = httptest.NewRecorder()
+	if err != nil {
+		t.Fatal("Creating 'POST /command' request failed!")
+	}
+	commandHandler().ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Command page didn't return %v", http.StatusOK)
+	}
+	if w.Body.String() != commands["meow"] {
+		t.Errorf("Response body didn't return %v", commands["meow"])
 	}
 }
